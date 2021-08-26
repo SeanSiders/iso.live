9b215b5685e18b3a9f8a6b5151e4bcb01d799d8c
Sean Siders <sean.siders@icloud.com>
2021-08-26 01:08:02 -0700

    Stylized Grass

---
+++ Assets/Scripts/InheritMaterials.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InheritMaterials : MonoBehaviour
+{
+    public Material material;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Renderer [] children = GetComponentsInChildren<Renderer>();        
+
+        foreach (Renderer r in children) r.material = material;
+    }
+}
---
+++ Assets/Scripts/InstantiateChildren.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateChildren : MonoBehaviour
+{
+    public GameObject [] children;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (GameObject child in children) Instantiate(child, transform);
+    }
+}
