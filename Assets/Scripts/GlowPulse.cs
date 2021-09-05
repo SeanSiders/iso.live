520687612fa3824af3be466286a926a7ba877179
Sean Siders <sean.siders@icloud.com>
2021-09-04 20:15:23 -0700

    Y-Plane movement and base running anim working with the new "Input System"
    Next we should set up an attack anim, and do some collision checking with capsule colliders! Not to mention health / attack / stamina base stats bound to a UI.

---
+++ Assets/Scripts/GlowPulse.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Playables;
+using UnityEngine;
+
+public class GlowPulse : MonoBehaviour
+{
+    public Renderer[] renderers;
+    public PlayableDirector director;
+
+    public float bpm = 130.0f;
+    public float intensityHigh = 3.0f;
+    public float intensityLow = 0.2f;
+
+    private float intensityLerp;
+    private float bps;
+
+    void Start()
+    {
+        intensityLerp = 0.0f;
+        UpdateBps();
+    }
+
+    void Update()
+    {
+        if (intensityLerp >= 1.0f) intensityLerp = 0.0f;
+
+        foreach (Renderer r in renderers)
+        {
+            r.material.SetFloat("GLOW_PULSE_SG_GLOW_INTENSITY", Mathf.Lerp(intensityHigh, intensityLow, intensityLerp));
+        }
+
+        intensityLerp += Time.smoothDeltaTime * bps;
+    }
+
+    void UpdateBps()
+    {
+        bps = bpm / 60.0f;
+    }
+}
